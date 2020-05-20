553d77d7926e1c1f65e30003e56f363ac6242de6
Eduardo2712002 <64163178+Eduardo2712002@users.noreply.github.com>
2020-05-20 15:02:24 -0500

    subida de la carpeta bex
    son todos los archivos que trabajamos en la clase

---
+++ BEX/pensamientomatematico.php
@@ -0,0 +1,272 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Editorial by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									<a href="index.html" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
+									<ul class="icons">
+										<li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+										<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+										<li><a href="#" class="icon fa-snapchat-ghost"><span class="label">Snapchat</span></a></li>
+										<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon fa-medium"><span class="label">Medium</span></a></li>
+									</ul>
+								</header>
+
+							<!-- Banner -->
+								<section id="banner">
+									<div class="content">
+										<header>
+											<h1>Pensamiento matemático<br />
+											by BEX</h1>
+											<p>BEX es para ti</p>
+										</header>
+										<p><li>Razonamiento Artimetico</li>
+										  <li> jerarquizacion de operaciones basicas</li>
+										  <li> operaciones conbinadas de suma</li>
+										   <li>resta, multiplicacion y division con numeros enteros</li>
+										   <li>problemas con suma, resta, multiplicacion y division con numeros decimales</li>
+										   <li>relaciones de proporcionalidad</li>
+										   <li>algebra</li>
+										   <li>productos notables</li>
+										   <li>ecuaciones</li>
+										   <li>representaciones graficas</li> 
+										   <li>medidas descriptivas</li>
+										   <li>probabilidad</li>
+										   <li>razonamiento geometrico</li>
+										   <li>linea recta</li>
+										   <li>trigonometria</li>
+										   <li>circulo trigonommetrico</li></p>
+										<ul class="actions">
+											<li><a href="#" class="button big">Learn More</a></li>
+										</ul>
+									</div>
+									<span class="image object">
+										<img src="images/bex.png" alt="" width="1034"
+										height="400" />
+									</span>
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Iformación</h2>
+									</header>
+									<div class="features">
+										<article>
+											<span class="icon fa-diamond"></span>
+											<div class="content">
+												<h3>Convocatoria UV 2020</h3>
+												<p> Revisa la convocatoria de nuevo ingreso para licenciatura y TSU.</p>
+												<p> Registro de aspirantes en la página desde el 12 de febrero. Para poder llenar tu registro debes de aceotar los términos y condiciones del servidor.</p>
+												<p> Ingresa al portal MiPago de la página de registro y realiza el depósito correspondiente por examen de ingreso $840.00 para aspirantes nacionales y $2535.00, para aspirantes extranjeros. Si no cuentas con tarjeta de débito o crédito, podrás pagar e imprimir una referencia de pago y pagar en una ventanilla bancaria. Para realizar el pago tienes del 12 febrero al 2 de abril.</p>
+												<p> EXAMEN DE ADMISIÓN: En tu credencial de ingreso al examen vendrá indicado el día y hora correspondiente.(Dias 25 y 26 de mayo).</p>
+												<p> Estos días 27-31 de abril, se llevarán a cabo los exámenes complementarios para los programas de artes, histotecnología y educación física. </p>
+												<p> Consulta los resultados de admisión el 21 de junio del 2020 en la página de la UV desde la 13:00 hrs. </p>
+
+											</div>
+										</article>
+										<article>
+											<span class="icon fa-paper-plane"></span>
+											<div class="content">
+												<h3>preguntas frecuentes</h3>
+												<p>puedes preguntarnos las dudas que tengas acerca de los apuntes o de como inscrbirse para el examen de ingreso a la uv.</p>
+											</div>
+										</article>
+										<article>
+											<span class="icon fa-rocket"></span>
+											<div class="content">
+												<h3>Comentarios</h3>
+												<p> Cualquier critica o recomendacion sobre el tenma que quieran compartirnos, para mejorar esta pagina.</p>
+											</div>
+										</article>
+								
+								</section>
+
+							<!-- Section -->
+								<section>
+									<header class="major">
+										<h2>Ipsum sed dolor</h2>
+									</header>
+									<div class="posts">
+										<article>
+											<a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+											<h3>Interdum aenean</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+											<h3>Nulla amet dolore</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+											<h3>Tempus ullamcorper</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+											<h3>Sed etiam facilis</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+											<h3>Feugiat lorem aenean</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+										<article>
+											<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+											<h3>Amet varius aliquam</h3>
+											<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+											<ul class="actions">
+												<li><a href="#" class="button">More</a></li>
+											</ul>
+										</article>
+									</div>
+								</section>
+
+						</div>
+					</div>
+
+				<!-- Sidebar -->
+					<div id="sidebar">
+						<div class="inner">
+
+							<!-- Search -->
+								<section id="search" class="alt">
+									<form method="post" action="#">
+										<input type="text" name="query" id="query" placeholder="Search" />
+									</form>
+								</section>
+
+							<!-- Menu -->
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+															<h2>Menu</h2>
+		</header>
+                  <ul>
+                    <li><a href="index.php">Home</a></li>
+                      <li><a href="estructuradelalengua.php">Estructura de la lengua</a></li>
+                   <li><a href="pensamientomatematico.php">Pensamiento matemático</a></li>
+                   <li><a href="pensamientoanalitico.php">Pensamiento analitico</a></li>
+                   <li><a href="logica.php">logica</a></li>
+                    <li>
+                      <span class="opener">Materias</span>
+                      <ul>
+                        <li><a href="#">Matemáticas</a></li>
+                        <li><a href="#">Español</a></li>
+                        <li><a href="#">Razonamiento</a></li>
+                        <li><a href="#">IQ</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#">UV</a></li>
+                    <li><a href="#">Simuladores (próximamente)</a></li>
+                    <li><a href="#">Acerca de</a></li>
+                    <li><a href="#">Contacto</a></li>
+                    <li>
+                      <span class="opener">Multimedia </span>
+                      <ul>
+                        <li><a href="#">Vídeos</a></li>
+                        <li><a href="#">Podcast (próximamente)</a></li>
+                        <li><a href="#">Tempus Magna</a></li>
+                        <li><a href="#">Feugiat Veroeros</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="#">Maximus Erat</a></li>
+                    <li><a href="#">Sapien Mauris</a></li>
+                    <li><a href="#">Amet Lacinia</a></li>
+                  </ul>
+                </nav>
+
+              <!-- Section -->
+                <section>
+                  <header class="major">
+                    <h2>Ante interdum</h2>
+                  </header>
+                  <div class="mini-posts">
+                    <article>
+                      <a href="#" class="image"><img src="images/pic07.jpg" alt="" /></a>
+                      <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+                    </article>
+                    <article>
+                      <a href="#" class="image"><img src="images/pic08.jpg" alt="" /></a>
+                      <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+                    </article>
+                    <article>
+                      <a href="#" class="image"><img src="images/pic09.jpg" alt="" /></a>
+                      <p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore aliquam.</p>
+                    </article>
+                  </div>
+                  <ul class="actions">
+                    <li><a href="#" class="button">More</a></li>
+                  </ul>
+                </section>
+
+              <!-- Section -->
+                <section>
+                  <header class="major">
+                    <h2>Get in touch</h2>
+                  </header>
+                  <p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+                  <ul class="contact">
+                    <li class="fa-envelope-o"><a href="#">information@untitled.tld</a></li>
+                    <li class="fa-phone">(000) 000-0000</li>
+                    <li class="fa-home">1234 Somewhere Road #8254<br />
+                    Nashville, TN 00000-0000</li>
+                  </ul>
+                </section>
+
+              <!-- Footer -->
+                <footer id="footer">
+                  <p class="copyright">&copy; Untitled. All rights reserved. Demo Images: <a href="https://unsplash.com">Unsplash</a>. Design: <a href="https://html5up.net">HTML5 UP</a>.</p>
+                </footer>
+
+            </div>
+          </div>
+
+      </div>
+
+    <!-- Scripts -->
+      <script src="assets/js/jquery.min.js"></script>
+      <script src="assets/js/browser.min.js"></script>
+      <script src="assets/js/breakpoints.min.js"></script>
+      <script src="assets/js/util.js"></script>
+      <script src="assets/js/main.js"></script>
+
+  </body>
+</html>
